cbb72d5661efb4feb49304bbe4b2b757f0f354c4
Josselin Maceda <joss@Josselins-MacBook-Pro.local>
2019-11-18 23:16:48 -0800

    Clock Joss

---
+++ clock.js
@@ -0,0 +1,16 @@
+var clockHour = function(clockHour){
+    for(let i= 0; i<clockHour.length; 1++){
+        console.log(clockHour[i])
+
+    }
+}
+var clockMin  = function(clockMin){
+    for(let i = 0; i<clockMin.length; 1++ ){
+        console.log(clockMin[i])
+    }
+}
+var clockSec = function(clockSec){
+    for(let i = 0; i<clockSec.length; i++){
+        console.log(clockSec[1])
+    }   
+}
